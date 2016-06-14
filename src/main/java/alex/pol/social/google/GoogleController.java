95ccd1871edc94a66467f32c10c4638beebd24fa
Dmitriy Litvinenko <doknenivtil@gmail.com>
2016-06-14 17:58:48 +0300

    Spring Social Google is refactored

--- src/main/java/alex/pol/social/google/GoogleController.java
+++ src/main/java/alex/pol/social/google/GoogleController.java
@@ -4,17 +4,8 @@ import alex.pol.domain.User;
 import alex.pol.repository.UserDataService;
 import alex.pol.repository.UserService;
 import alex.pol.util.validation.UserValid;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.BasicResponseHandler;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.connect.Connection;
-import org.springframework.social.facebook.api.Facebook;
-import org.springframework.social.facebook.connect.FacebookConnectionFactory;
 import org.springframework.social.google.api.Google;
 import org.springframework.social.google.connect.GoogleConnectionFactory;
 import org.springframework.social.oauth2.AccessGrant;
@@ -24,13 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.ServletException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
 import java.sql.SQLException;
-import java.util.Enumeration;
 import java.util.List;
 
 @Controller
@@ -55,19 +44,18 @@ public class GoogleController {
 
 
     @RequestMapping(value = "/googleLogin", method = RequestMethod.GET)
-    public String loginWithFacebook(HttpServletRequest request) {
-
-        HttpSession session = request.getSession();
+    public String loginWithGoogle(HttpServletRequest request) {
+        //HttpSession session = request.getSession();
         //String sessionId = session.getId();
         String returnValue =
-                "https://accounts.google.com/o/oauth2/v2/auth?scope=email+profile&"//email,profile&"//%20
-                        + "redirect_uri=" + REDIRECT_URL//"state=%2Fprofile&"
+                "https://accounts.google.com/o/oauth2/v2/auth?scope=email+profile&"
+                        + "redirect_uri=" + REDIRECT_URL
                         + "&response_type=code&client_id=" + APP_ID;
         return "redirect:" + returnValue;
     }
 
     @RequestMapping(value = "/googleCallback", method = RequestMethod.GET)
-    public ModelAndView getToken(HttpServletRequest request, HttpServletResponse response) throws SQLException {
+    public ModelAndView getAccessToken(HttpServletRequest request, HttpServletResponse response) throws SQLException {
         HttpSession httpSession = request.getSession();
         String authorizationCode = request.getParameter("code");
         GoogleConnectionFactory googleConnectionFactory =
